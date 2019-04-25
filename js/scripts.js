f10883657d7d3adfcd15daddc86c9e1d7d7f191d
James Coates <James@Lydias-MacBook-Pro.local>
2019-04-25 20:01:19 +0100

    refactor js inside IIFE and composing functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,112 @@
-var repository;
+var app = (function(){
 
-repository = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['Grass', 'Poison']
-  },
-  {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['Fire']
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['Water']
-  },
-  {
-    name: 'Snorlax',
-    height: 2.1,
-    types: ['Normal']
+  var pokemonRepository = (function(){
+
+    var pokemon = [
+      {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['Grass', 'Poison']
+      },
+      {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['Fire']
+      },
+      {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['Water']
+      },
+      {
+        name: 'Snorlax',
+        height: 2.1,
+        types: ['Normal']
+      }
+    ];
+
+    function add(pokemon){
+      pokemon.push(pokemon);
+    }
+
+    function getAll(){
+      return pokemon;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    }
+
+  })();
+
+  openContainer();
+
+  pokemonRepository.getAll().forEach(function(pokemon){
+
+    // document.write(getPokemonDescription(pokemon));
+
+    writePokemonDescription(pokemon);
+    // writePokemonTypes(pokemon);
+
+  });
+
+  closeContainer();
+
+
+  // Define Global Functions
+
+  function openContainer(){
+    document.write('<div class="list-container">')
+    document.write('<h2>List of Pokèmon</h2>')
   }
-];
 
-// Some temporary formatting
-document.write('<div class="list-container">')
-document.write('<h2>List of Pokèmon</h2>')
-
-// Loop through Pokemon repository and print data
-for (var i = 0; i < repository.length; i++){
-  var name = repository[i].name;
-  var height = repository[i].height;
-  var types = repository[i].types;
-  var output = '<p>' + name + ' (height: ' + height + 'm)';
-
-  // Highlight large Pokemon
-  if (height > 2){
-    output = output + ' - Wow that\'s Large! </p>'; 
-  } else {
-    output = output + '</p>';
+  function closeContainer(){
+    document.write('</div>');
   }
 
-  document.write(output);
-
-  // Print types
-  document.write('<p>Type: ');
-  for (var j = 0; j < types.length; j++){
-    var type = types[j];
-    var numberOfTypes = types.length - 1;
-    var typeClass = 'type type__' + type.toLowerCase();
-    var typeOutput = '<span class="' + typeClass + '">' + type + '</span>';
-
-    var output = j < numberOfTypes ? typeOutput + ', ' : typeOutput;
-    document.write(output);
+  function returnParagraph(paragraph){
+    return '<p>' + paragraph + '</p>'
   }
-  document.write('</p>');
-  document.write('<br>');
-}
 
-document.write('</div>');
+  function getHeightComment(height){
+    return height > 2 ? ' - Wow that\'s Large!' : '';
+  }
+
+  function getHeightDescription(height){
+    var heightComment = getHeightComment(height);
+    return '(height: ' + height + 'm)' + heightComment;
+  }
+
+  function printTypes(pokemon){
+    document.write('<p>Type: ');
+    
+    document.write('</p>');
+    document.write('<br>');
+  }
+
+  function getPokemonTypes(pokemon){
+    var pokemonTypes = '';
+    for (var i = 0; i < pokemon.types.length; i++){
+      var type = pokemon.types[i];
+      var numberOfTypes = pokemon.types.length - 1;
+      var typeClass = 'type type__' + type.toLowerCase();
+      var typeOutput = '<span class="' + typeClass + '">' + type + '</span>';
+      var pokemonType = pokemonTypes + i < numberOfTypes ? typeOutput + ', ' : typeOutput;
+      pokemonTypes += pokemonType;
+    }
+    return pokemonTypes;
+  }
+
+  function getPokemonDescription(pokemon){
+    var pokemonHeightDescription = getHeightDescription(pokemon.height);
+    return pokemon.name + ' ' + pokemonHeightDescription;
+  }
+
+  function writePokemonDescription(pokemon){
+    var pokemonDescription = getPokemonDescription(pokemon);
+    var pokemonTypes = getPokemonTypes(pokemon);
+    return document.write(returnParagraph(pokemonDescription) + returnParagraph(pokemonTypes) + '<br>');
+  }
+
+})();
