b5198a4bb28d141a349abfd6697f38ae77833d58
James Coates <James@Lydias-MacBook-Pro.local>
2019-04-30 20:49:39 +0100

    full visual redesign app

--- js/scripts.js
+++ js/scripts.js
@@ -109,3 +109,121 @@ var testing = (function(){
   }
 
 })();
+
+
+
+
+/* Previous Version
+**************************************
+var testing = (function(){
+
+  var pokemonRepository = (function(){
+
+    var pokemon = [
+      {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['Grass', 'Poison']
+      },
+      {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['Fire']
+      },
+      {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['Water']
+      },
+      {
+        name: 'Snorlax',
+        height: 2.1,
+        types: ['Normal']
+      }
+    ];
+
+    function add(addedPokemon){
+      if (typeof addedPokemon === 'object'){
+        pokemon.push(pokemon);
+      } else {
+        console.log('Must be an object');
+      }
+    }
+
+    function getAll(){
+      return pokemon;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    }
+
+  })();
+
+  openContainer();
+
+  pokemonRepository.getAll().forEach(function(pokemon){
+    writePokemonDescription(pokemon);
+  });
+
+  closeContainer();
+
+  // ***********************
+  // Define Global Functions
+
+  function openContainer(){
+    document.write('<div class="list-container">');
+    document.write('<h2>List of Pokèmon</h2>');
+  }
+
+  function closeContainer(){
+    document.write('</div>');
+  }
+
+  function returnParagraph(paragraph){
+    return '<p>' + paragraph + '</p>';
+  }
+
+  function getHeightComment(height){
+    return height > 2 ? ' - Wow that\'s Large!' : '';
+  }
+
+  function getHeightDescription(height){
+    var heightComment = getHeightComment(height);
+    return '(height: ' + height + 'm)' + heightComment;
+  }
+
+  function printTypes(pokemon){
+    document.write('<p>Type: ');
+    
+    document.write('</p>');
+    document.write('<br>');
+  }
+
+  function getPokemonTypes(pokemon){
+    var pokemonTypes = '';
+    for (var i = 0; i < pokemon.types.length; i++){
+      var type = pokemon.types[i];
+      var numberOfTypes = pokemon.types.length - 1;
+      var typeClass = 'type type__' + type.toLowerCase();
+      var typeOutput = '<span class="' + typeClass + '">' + type + '</span>';
+      var pokemonType = pokemonTypes + i < numberOfTypes ? typeOutput + ', ' : typeOutput;
+      pokemonTypes += pokemonType;
+    }
+    return pokemonTypes;
+  }
+
+  function getPokemonDescription(pokemon){
+    var pokemonHeightDescription = getHeightDescription(pokemon.height);
+    return pokemon.name + ' ' + pokemonHeightDescription;
+  }
+
+  function writePokemonDescription(pokemon){
+    var pokemonDescription = getPokemonDescription(pokemon);
+    var pokemonTypes = getPokemonTypes(pokemon);
+    return document.write(returnParagraph(pokemonDescription) + returnParagraph(pokemonTypes) + '<br>');
+  }
+
+})();
+*/
